3f591d68d7636d3461a125d546b26732d71cc903
Blaine Love <blainelove94@gmail.com>
2021-02-21 22:22:25 -0500

    commit tree 6c9492d58d015a11c3a128248273b439a2db1706

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+let drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name) {
+    drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+    drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver(name) {
+    drivers.pop(name);
+}
+
+function destructivelyRemoveFirstDriver(name) {
+    drivers.shift(name);
+}
+
+function destructivelyAppendDriver(name) {
+    drivers.push(name);
+}
+
+// function destructivelyAppendDriver(name) {
+//     drivers(name);
+// }
+// const alldrivers = [...drivers, "Broom"]
+
+// const realdrivers = ["Arnold", ...drivers]
+
+
